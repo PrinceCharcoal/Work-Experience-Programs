35ed9a41d27716e380ede15aff37f3fd5ccce171
Cole Boardman <ColeBoardman@chessdigital.co.uk>
2018-07-10 10:30:37 +0100

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Palindrome
+{
+    class Palindrome
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter any word/phrase to determine whether or not it is a palindrome");
+            string s = Console.ReadLine();
+            string s1 = s.ToLower();
+            string s2 = null; 
+            string s3 = null;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((int)s1[i] >= 97 && (int)s1[i] <= 122)
+                {
+                    s2 += s1[i];
+                }
+            }
+            Console.WriteLine(s2);
+            int o = s2.Length - 1;
+            for (int i = 0; i < s2.Length; i++)
+            {
+                int j = o - i;
+                s3 += s2[j];
+            }
+            if (s3 == s2)
+            {
+                Console.WriteLine("{0} is a palindrome!", s);
+            }
+            else
+            {
+                Console.WriteLine("'{0}' is not a palindrome!", s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Text Adventure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Text_Adventure
+{
+    class Core
+    {
+        
+
+    }
+    class Settings
+    {
+        public void Intro()
+        {
+            Console.WriteLine("You are awake.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var  = new Settings();
+            t.Intro();
+            Console.ReadKey();
+        }
+    }
+}
